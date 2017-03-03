537e28697b6f04ddeaa6066ff52034f73e3c6751
florian leroux <f.j.j.leroux@gmail.com>
2017-03-03 10:23:18 +0100

    add google maps with api

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+	$('#map').googleMap({
+		zoom: 10, // Initial zoom level (optional)
+		coords: [48.895651, 2.290569], // Map center (optional)
+		type: 'ROADMAP' // Map type (optional)
+	});
+})
