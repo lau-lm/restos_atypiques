72cd86d9adbf7ed7099bfb97c2b568076018b1fa
florian leroux <f.j.j.leroux@gmail.com>
2017-03-05 14:00:03 +0100

    add legend on markers + concept before map

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,27 +9,39 @@ $(document).ready(function () {
 	// Marker 1
 	$('#map').addMarker({
 		coords: [45.746404, 4.824766],
+		title: 'Restaurant atypique Lyon',
+		text: `Toujours plus d'atypique, l'atypique c'est trop bien. Viendez !!`,
 	});
 
 	// Marker 2
 	$('#map').addMarker({
 		coords: [48.4735294, 1.0098665],
+		title: 'Restaurant atypique La Loupe',
+		text: `Toujours plus d'atypique, l'atypique c'est trop bien. Viendez !!`,
 	});
 
 	// Marker 3
 	$('#map').addMarker({
 		coords: [43.601519, 1.439835],
+		title: 'Restaurant atypique Toulouse',
+		text: `Toujours plus d'atypique, l'atypique c'est trop bien. Viendez !!`,
 	});
 	// Marker 4
 	$('#map').addMarker({
 		coords: [47.8938701, 1.8920055],
+		title: 'Restaurant atypique Orléans',
+		text: `Toujours plus d'atypique, l'atypique c'est trop bien. Viendez !!`,
 	});
 	// Marker 5
 	$('#map').addMarker({
 		coords: [44.8336476, -0.5682131],
+		title: 'Restaurant atypique Bordeaux',
+		text: `Toujours plus d'atypique, l'atypique c'est trop bien. Viendez !!`,
 	});
 	// Marker 6
 	$('#map').addMarker({
 		coords: [48.4037414, 2.6921273],
+		title: 'Restaurant atypique Fontainebleau',
+		text: `Toujours plus d'atypique, l'atypique c'est trop bien. Viendez !!`,
 	});
 });
